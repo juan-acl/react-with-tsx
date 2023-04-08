c5d30541f5bd05e80bbffe2f2dd4ac6113e8f570
develop <dev-juan-eloper@gmail.com>
2023-04-08 12:41:37 -0600

    #2 states with types

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,7 +1,14 @@
+type Props = { 
+  image: string,
+}
 
 
-export const RandomFox = (props: {image:string} ):JSX.Element => { 
+export const RandomFox = ({image}: Props ):JSX.Element => { 
   //const image:string = 
-  return <img width={320} height="auto" className="rounded" src={props.image}/>
+  return (
+    <>
+    <img width={320} height="auto" className="rounded" src={image} />
+    </>
+  )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import { RandomFox } from "@/components/RandomFox"
+import { useState } from "react";
 
 export default function Home() {
 
-const random = () => Math.floor(Math.random() * 123) +1;
-
+  const random = () => Math.floor(Math.random() * 123) +1;
+  
+  const [images, setImages] = useState<string[]>([
+`https://randomFox.ca/images/${random()}.jpg`,
+  ]);
   return (
     <main>
       <h1 className="text-3xl font-bold underline" >holaaaa</h1>
-      <RandomFox image= {`https://randomFox.ca/images/${random()}.jpg`}/>
+      {images.map((item, index) => (
+      <div key={index} >
+      <RandomFox image= {item}/>
+      </div>
+      ))}
     </main>
   )
 }
