2529424659ba0177b02308b4f7a81e56404ca1e0
develop <dev-juan-eloper@gmail.com>
2023-04-08 12:01:11 -0600

    Components with types

---
+++ components/RandomFox.tsx
@@ -0,0 +1,18 @@
+import type { FunctionComponent, FC } from "react"
+
+export const RandomFox = () => { 
+  return <img />
+}
+
+export const RandomFox1 = ():JSX.Element => { 
+  return <img />
+}
+
+export const RandomFox2: FunctionComponent = () => { 
+  return <img />
+}
+
+export const RandomFox3: FC = () => {
+  return <img />
+}
+
